6d91125f023af899e4f4a5fb0cac3c291da1199d
João Justo <jpjustonunes@gmail.com>
2021-01-16 23:29:24 +0000

    WIP

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,17 @@
 <style>
   main {
     text-align: center;
-    padding: 1em;
+    padding: 0;
     max-width: 240px;
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  div {
+    min-height: 50vh;
+  }
+
+  :global(.ol-viewport) {
+    min-height: 50vh;
   }
 
   @media (min-width: 640px) {
@@ -21,13 +22,74 @@
 </style>
 
 <script>
-  export let name;
+  import { onMount } from 'svelte';
+
+  import { Map, View, Feature, Geolocation } from 'ol';
+  import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+  import Point from 'ol/geom/Point';
+  import { OSM, Vector as VectorSource } from 'ol/source';
+  import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+
+  let map = null;
+  let geolocation = null;
+  const view = new View({
+    center: [0, 0],
+    zoom: 2,
+  });
+
+  onMount(() => {
+    map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: document.getElementById('map'),
+      view: view,
+    });
+
+    geolocation = new Geolocation({
+      trackingOptions: { enableHighAccuracy: true },
+      projection: view.getProjection(),
+    });
+
+    geolocation.setTracking(true);
+
+    geolocation.on('change:position', function () {
+      var coordinates = geolocation.getPosition();
+      positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
+    });
+
+    let positionFeature = new Feature();
+    positionFeature.setStyle(
+      new Style({
+        image: new CircleStyle({
+          radius: 6,
+          fill: new Fill({
+            color: '#3399CC',
+          }),
+          stroke: new Stroke({
+            color: '#fff',
+            width: 2,
+          }),
+        }),
+      })
+    );
+
+    var accuracyFeature = new Feature();
+    geolocation.on('change:accuracyGeometry', function () {
+      accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+    });
+
+    new VectorLayer({
+      map: map,
+      source: new VectorSource({
+        features: [accuracyFeature, positionFeature],
+      }),
+    });
+  });
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <div id="map" class="map"></div>
 </main>
